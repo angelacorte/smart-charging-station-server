e0a4f1cee283c1f006782ad640c28b1972c67ba2
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-30 18:41:51 +0200

    fix: change db name

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports={
-    url: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSW}@${process.env.DB_NAME}.huxz3oi.mongodb.net/equilessons?retryWrites=true&w=majority`
+    url: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSW}@${process.env.DB_NAME}.huxz3oi.mongodb.net/smartchargingstations?retryWrites=true&w=majority`
 };
