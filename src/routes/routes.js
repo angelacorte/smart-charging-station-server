c212eb0dca0e02c040ec5fae725b41d610d2b98d
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-09-01 16:04:24 +0200

    feat: add routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 const loginController = require('../controller/login')
 const signupController = require('../controller/signup')
+const preferencesController = require('../controller/preferences')
 
 module.exports = function (app){
 
@@ -9,4 +10,9 @@ module.exports = function (app){
     app.route('/auth/signup')
         .post(signupController.signup);
 
+    app.route('/addFavourite')
+        .post(preferencesController.addFavourite)
+
+    app.route('/removeFavourite')
+        .post(preferencesController.removeFavourite)
 }
